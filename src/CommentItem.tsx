5a2c14c47e22e2a803b8e0eb4f21053c82d4ce83
Martiyn <martiynwork@gmail.com>
2022-08-17 21:57:25 +0300

    removed console.log

--- src/CommentItem.tsx
+++ src/CommentItem.tsx
@@ -29,7 +29,6 @@ export const CommentItem = ({ comment, ...rest }: CommentItemProps) => {
   }
 
   function handleCommentEdit() {
-    console.log('az sum itemcheto', comment)
     rest.onEditComment(comment);
   }
 
