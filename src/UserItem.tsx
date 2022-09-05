1a3004be4bf9e9af0bfe95e7acf4d45fcf9f1edd
Martiyn <martiynwork@gmail.com>
2022-09-05 11:41:17 +0300

    commit pre-cardification

--- src/UserItem.tsx
+++ src/UserItem.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Optional, UserListener } from "./shared-types";
 import { User, UserGender, UserRole, UserStatus } from "./users-model";
 import "./UserItem.css";
+
+const CARD_HEADER_HEIGHT = 60;
+const CARD_CONTENT_HEIGHT = 100;
 
 interface UserItemProps {
   user: User;
@@ -10,12 +28,33 @@ interface UserItemProps {
   onEditUser: UserListener;
 }
 
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 export const UserItem = ({
   user,
   loggedUser,
   onDeleteUser,
   onEditUser,
 }: UserItemProps) => {
+  
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   function handleEdit() {
     onEditUser(user);
   }
